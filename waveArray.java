da586aefb3ae09005b71ccf3b3dda1591a487d07
Danusshkumar <112367768+Danusshkumar@users.noreply.github.com>
2023-08-26 21:38:17 +0530

    Create waveArray.java

---
+++ waveArray.java
@@ -0,0 +1,29 @@
+/*
+
+Wave Array
+Given a sorted array arr[] of distinct integers. Sort the array into a wave-like array(In Place).
+In other words, arrange the elements into a sequence such that arr[1] >= arr[2] <= arr[3] >= arr[4] <= arr[5].....
+
+If there are multiple solutions, find the lexicographically smallest one.
+
+Note:The given array is sorted in ascending order, and you don't need to return anything to make changes in the original array itself.
+
+Explanation:
+The problem is to just swap the adjacent elements in the arrays (swapping even and odd place elements)
+Here is the solution for it
+
+*/
+
+class Solution {
+    public static void convertToWave(int n, int[] a) {
+        
+        for(int i = 0;i<n;i+=2){
+            if(i != n-1){
+                int temp = a[i];
+                a[i] = a[i+1];
+                a[i+1] = temp;
+            }
+        }
+    }
+}
+        
